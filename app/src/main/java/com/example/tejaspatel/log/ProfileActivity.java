3f7dc73dd03f0b5f6c9e0e50bedc8d070b52d9fa
yashkpatel <ykp.2604@gmail.com>
2017-10-04 15:37:19 +0530

    icons updated in profile activity

--- app/src/main/java/com/example/tejaspatel/log/ProfileActivity.java
+++ app/src/main/java/com/example/tejaspatel/log/ProfileActivity.java
@@ -60,9 +60,9 @@ public class ProfileActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
 
         tabLayout.getTabAt(0).setIcon(R.mipmap.ic_post);
-        tabLayout.getTabAt(1).setIcon(R.mipmap.ic_post);
-        tabLayout.getTabAt(2).setIcon(R.mipmap.ic_post);
-        tabLayout.getTabAt(3).setIcon(R.mipmap.ic_post);
+        tabLayout.getTabAt(1).setIcon(R.mipmap.ic_img_request);
+        tabLayout.getTabAt(2).setIcon(R.mipmap.ic_date);
+        tabLayout.getTabAt(3).setIcon(R.mipmap.ic_img_user);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
